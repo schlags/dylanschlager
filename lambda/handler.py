32d0c7cb73c7b7e92800b7a60e741b60cc9a5332
Dylan Schlager <mail@dylanschlager.com>
2022-05-31 17:52:45 -0600

    Fixing lambda and site form with dynamic data pull for attendees

--- lambda/handler.py
+++ lambda/handler.py
@@ -8,7 +8,7 @@ import time
 # create a DynamoDB object using the AWS SDK
 dynamodb = boto3.resource('dynamodb')
 # use the DynamoDB object to select our table
-table = dynamodb.Table('HelloWorldDatabase')
+table = dynamodb.Table('BirthdaySignup')
 
 # define the handler function that the Lambda service will use as an entry point
 def lambda_handler(event, context):
@@ -18,7 +18,6 @@ def lambda_handler(event, context):
 # extract values from the event object we got from the Lambda service and store in a variable
     try:
         name = event['firstName'] +' '+ event['lastName']
-        bringing = event['brings']
     except:
         people = table.scan()['Items']
         try:
@@ -35,15 +34,10 @@ def lambda_handler(event, context):
 # write name and time to the DynamoDB table using the object we instantiated and save response in a variable
     response = table.put_item(
         Item={
-            'ID': name,
-            'Bringing': bringing,
+            'id': name,
             'Signed Up Time':str(for_js)
             })
 # return a properly formatted JSON object
-    if bringing != "":
-        bringstring = f" You are now bringing {bringing}."
-    else:
-        bringstring = ""
     return {
         'statusCode': 201,
         'headers': {
@@ -51,14 +45,11 @@ def lambda_handler(event, context):
             'Access-Control-Allow-Origin': '*',
             'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
         },
-        'body': json.dumps(f'Welcome, {name}.{bringstring} Please refresh the page.')
+        'body': json.dumps(f'Welcome, {name}. See you at the barcrawl!')
     }
 
-print(lambda_handler("", ""))
+
 print(lambda_handler({
     "firstName": "Dylan",
     "lastName": "Schlager",
-    "brings": "Old Fashioned's"
-    },""
-    )
-)
+    },""))
