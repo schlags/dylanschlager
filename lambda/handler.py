0f27ac9844506c64ba86ff15fc22ce27ee4c616d
Dylan Schlager <mail@dylanschlager.com>
2022-06-02 10:22:35 -0600

    Fixing boto import

--- lambda/handler.py
+++ lambda/handler.py
@@ -1,9 +1,7 @@
-# import the json utility package since we will be working with a JSON object
 import json
+import time
 # import the AWS SDK (for Python the package name is boto3)
 import boto3
-# import two packages to help us with dates and date formatting
-import time
 
 # create a DynamoDB object using the AWS SDK
 dynamodb = boto3.resource('dynamodb')
